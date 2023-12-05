1e7aad57d81829bc468305092e609fdf389757bf
KonradLewickiGit <konrad.lewicki00@gmail.com>
2023-12-05 02:58:09 +0100

    fixed chat cors and added creating chat for product

--- backend/src/main/java/com/kkicks/backend/dao/ChatDao.java
+++ backend/src/main/java/com/kkicks/backend/dao/ChatDao.java
@@ -1,12 +1,11 @@
 package com.kkicks.backend.dao;
 
 import com.kkicks.backend.entity.Chat.Chat;
+import com.kkicks.backend.entity.Product.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Optional;
-
 @Repository
 public interface ChatDao  extends JpaRepository<Chat,Long> {
-    Optional<Chat> findByProductId(Long productId);
+    Chat findByProduct(Product product);
 }
