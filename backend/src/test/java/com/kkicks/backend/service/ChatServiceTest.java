3e5fb7b4a3a94770a0adaf6bfcf7a77527220415
KonradLewickiGit <konrad.lewicki00@gmail.com>
2023-12-12 12:50:38 +0100

    changed test cuz of new implementation

--- backend/src/test/java/com/kkicks/backend/service/ChatServiceTest.java
+++ backend/src/test/java/com/kkicks/backend/service/ChatServiceTest.java
@@ -1,61 +1,71 @@
-//package com.kkicks.backend.service;
-//
-//import com.kkicks.backend.dao.ChatDao;
-//import com.kkicks.backend.entity.Chat.Chat;
-//import jakarta.persistence.EntityNotFoundException;
-//import org.junit.jupiter.api.BeforeEach;
-//import org.junit.jupiter.api.Test;
-//import org.mockito.InjectMocks;
-//import org.mockito.Mock;
-//import org.mockito.MockitoAnnotations;
-//
-//import java.util.Optional;
-//
-//import static org.junit.jupiter.api.Assertions.*;
-//import static org.mockito.ArgumentMatchers.eq;
-//import static org.mockito.Mockito.*;
-//
-//class ChatServiceTest {
-//    @InjectMocks
-//    private ChatService chatService;
-//
-//    @Mock
-//    private ChatDao chatDao;
-//
-//    // Initialize mocks
-//    @BeforeEach
-//    void setUp() {
-//        MockitoAnnotations.openMocks(this);
-//    }
-//
-//    @Test
-//    void testGetChatByProductId_Exists() {
-//        // Arrange
-//        Long productId = 1L;
-//        Chat expectedChat = new Chat();
-//        when(chatDao.findByProductId(eq(productId))).thenReturn(Optional.of(expectedChat));
-//
-//        // Act
-//        Chat actualChat = chatService.getChatByProductId(productId);
-//
-//        // Assert
-//        assertNotNull(actualChat);
-//        assertEquals(expectedChat, actualChat);
-//        verify(chatDao, times(1)).findByProductId(eq(productId));
-//    }
-//
-//    @Test
-//    void testGetChatByProductId_NotExists() {
-//        // Arrange
-//        Long productId = 1L;
-//        when(chatDao.findByProductId(eq(productId))).thenReturn(Optional.empty());
-//
-//        // Act and Assert
-//        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () ->
-//                chatService.getChatByProductId(productId));
-//
-//        assertEquals("Chat not found", exception.getMessage());
-//        verify(chatDao, times(1)).findByProductId(eq(productId));
-//    }
-//
-//}
+package com.kkicks.backend.service;
+
+import com.kkicks.backend.dao.ChatDao;
+import com.kkicks.backend.dao.ProductDao;
+import com.kkicks.backend.entity.Chat.Chat;
+import com.kkicks.backend.entity.Product.Product;
+import jakarta.persistence.EntityNotFoundException;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.*;
+
+class ChatServiceTest {
+    @InjectMocks
+    private ChatService chatService;
+
+    @Mock
+    private ChatDao chatDao;
+    @Mock
+    private ProductDao productDao;
+
+    // Initialize mocks
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+    }
+
+    @Test
+    void testGetChatByProductId_Exists() {
+        // Arrange
+        Long productId = 1L;
+        Product product = new Product();
+        product.setId(productId);
+        Chat expectedChat = new Chat(null,product);
+        when(productDao.findById(eq(productId))).thenReturn(Optional.of(product));
+        when(chatDao.findChatByProduct(eq(product))).thenReturn(Optional.of(expectedChat));
+
+        // Act
+        Chat actualChat = chatService.getChatByProductId(productId);
+
+        // Assert
+        assertNotNull(actualChat);
+        assertEquals(expectedChat, actualChat);
+        verify(chatDao, times(1)).findChatByProduct(eq(product));
+    }
+
+    @Test
+    void testGetChatByProductId_NotExists() {
+        // Arrange
+        Long productId = 1L;
+        Product product = new Product();
+        product.setId(productId);
+        when(productDao.findById(eq(productId))).thenReturn(Optional.of(product));
+        when(chatDao.findChatByProduct(eq(product))).thenReturn(Optional.empty());
+
+        // Act and Assert
+        EntityNotFoundException exception = assertThrows(EntityNotFoundException.class, () ->
+                chatService.getChatByProductId(productId));
+
+        assertEquals("chat not found", exception.getMessage());
+        verify(chatDao, times(1)).findChatByProduct(eq(product));
+    }
+
+}
